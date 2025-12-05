2d730055c727af4aaa7e7c92c5d2c621cf58d3ac
athenszz00 <athenszz00@gmail.com>
2025-12-05 09:05:15 +0700

    fix: update db connection for Railway

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,15 +4,16 @@ let globalWithPool = globalThis;
 
 if (!globalWithPool.pool) {
   globalWithPool.pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "ocare",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT ?? 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
   });
 }
 
-// ✅ Export pool langsung
+
 export const pool = globalWithPool.pool;
